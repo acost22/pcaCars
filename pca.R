e052dc3b04bbf96bbfd2d72f43e2707ce4fc83ae
Daniella Acosta <daniella.c.acosta@gmail.com>
2017-11-05 18:18:44 -0500

    PCA for mtcars
    Initial comment of PCA R code

---
+++ pca.R
@@ -0,0 +1,16 @@
+# navigate to pcaCars repo; set as working directory
+setwd('~/Documents/GitHub/pcaCars')
+
+# cor = TRUE indicates that PCA is performed on
+# standardized data (mean = 0, variance = 1)
+pcasCars <- princomp(mtcars, cor = TRUE)
+
+# view objects stored in pcaCars
+names(pcasCars)
+
+# proportion of variance explained
+summary(pcasCars)
+
+# scree plot 
+plot(pcasCars, type = "l")
+
